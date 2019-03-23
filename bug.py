9dc035df4e688455fcfc4dcc4055f5a43654fc18
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2019-03-23 17:04:14 +0700

    Add files via upload

---
+++ bug.py
@@ -0,0 +1,27 @@
+
+import requests
+
+# Tools Priv8 Phreaking Internet Provider Indonesia 
+# Output Bug200ok.txt 
+# Tested : Windows & Termux Working
+# Regard Boychongzen aka Xroot
+
+
+print ('''\033[1m\033[93m[\033[91m!\033[93m] Tools Priv8 Phreaking Internet Provider Indonesia [ SALE ] \033[1;37m
+Welcombek My Tools \n \033[1;31m
+
+__________                 _________ __          __                
+\______   \__ __  ____    /   _____//  |______ _/  |_ __ __  ______
+ |    |  _/  |  \/ ___\   \_____  \\   __\__  \\   __\  |  \/  ___/
+ |    |   \  |  / /_/  >  /        \|  |  / __ \|  | |  |  /\___ \ 
+ |______  /____/\___  /  /_______  /|__| (____  /__| |____//____  >
+        \/     /_____/           \/           \/                \/ 
+            _________   _____  .____     ___________               
+           /   _____/  /  _  \ |    |    \_   _____/               
+           \_____  \  /  /_\  \|    |     |    __)_                
+           /        \/    |    \    |___  |        \               
+          /_______  /\____|__  /_______ \/_______  /               
+                  \/         \/        \/        \/  \033[1;34mAuthor \033[91m:\033[37m Boychongzen aka Xroot                                  
+
+\033[1;34mMy \033[91m:\033[37mFacebook\033[91m:\033[37m https://web.facebook.com/xroot.xroot.7 > Xroot  \n \033[1;34m    
+ ''')
